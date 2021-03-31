1230cf81b20ee20ffd839a7bcfb3a781b8ff64d2
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-03-31 18:31:09 +0900

    Add files via upload

--- namuwiki_preprocessing.py
+++ namuwiki_preprocessing.py
@@ -10,9 +10,13 @@ import pickle
 import argparse
 import pandas as pd
 
+
+# 길이가 32 이상의 것으로 하고, 100만개만 뽑아내자
+# 한글 숫자만 살리기
 parser = argparse.ArgumentParser()
 parser.add_argument("--data", type=str, default = './data/namuwiki.txt')
-parser.add_argument("--output_data", type=str, default = './data/preprocessed_data_2.txt')
+parser.add_argument("--output_data", type=str, default = './data/preprocessed_data_namuwiki.txt')
+parser.add_argument("--sentence_count", type=int, default = 1000000)
 
 def substitute(i):
     try:
@@ -20,17 +24,19 @@ def substitute(i):
     except:
         return ''
     
-class preprocessing(object):
-    def __init__(self):
-        pass
-    def transform(self):
-        data = pd.read_csv(args.data,sep='\n',header=0,encoding='utf-8')
-        # 한글,. ?숫자 외는 제거
-        data['document'] = data['document'].apply(lambda i : substitute(i))
-        new_data = data.loc[data['document']!='','document']
-        new_data.to_csv(args.output_data,sep='\n',index=False,header=False)
-
 if __name__=='__main__':
     args = parser.parse_args()
-    p = preprocessing()
-    p.transform()    
+    F = open(args.data,'r',encoding='utf-8')
+    f = open(args.output_data,'w',encoding='utf-8')
+    n = 0
+    while True:
+        i = F.readline()
+        i = re.sub('[^ㄱ-ㅎ가-힇 ,.?0-9]+','',i)
+        if len(i)>32:
+            f.write(i+'\n')
+            n+=1
+        if n==args.sentence_count:
+            F.close()
+            f.close()
+            break
+        
