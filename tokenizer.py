bea2d9f8ce5f435aa94d7a41f4ce23fca58406a2
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-04-15 17:19:40 +0900

    Add files via upload

--- tokenizer.py
+++ tokenizer.py
@@ -7,8 +7,9 @@ import argparse
 import os
 from tokenizers import BertWordPieceTokenizer
 parser = argparse.ArgumentParser()
-parser.add_argument("--corpus_file", type=str, default = './data/preprocessed_data_2.txt')
-parser.add_argument("--vocab_size", type=int, default=8000) # 만들 Vocab의 숫자 
+parser.add_argument("--corpus_file", type=str, default = './data/namuwiki/namuwiki.txt')
+parser.add_argument("--tokenizer_model", type=str, default = './tokenizer_model')
+parser.add_argument("--vocab_size", type=int, default=16000) # 만들 Vocab의 숫자 
 parser.add_argument("--limit_alphabet", type=int, default=6000)
 parser.add_argument("--min_freq", type=int, default=5)
 
@@ -20,15 +21,14 @@ if __name__=="__main__":
                                    strip_accents = False,
                                    lowercase = False,
                                    wordpieces_prefix='##'
-                                   
-                                   )
+                                  )
      tokenizer.train(
         files = [args.corpus_file],
         limit_alphabet=args.limit_alphabet,
         vocab_size = args.vocab_size,
         min_frequency = args.min_freq)
-     if not os.path.isdir('./tokenizer_model2'):
-         os.mkdir('./tokenizer_model2')
-     tokenizer.save_model('./tokenizer_model2')
+     if not os.path.isdir(args.tokenizer_model):
+         os.mkdir(args.tokenizer_model)
+     tokenizer.save_model(args.tokenizer_model)
       
 
